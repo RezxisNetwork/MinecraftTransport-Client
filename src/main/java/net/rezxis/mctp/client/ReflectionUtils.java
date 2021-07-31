96b8c4b7c7f48af0ace47c16a1af48717050c324
cursan0409@gmail.com <torakichi0131!>
2021-07-31 23:03:49 +0900

    support jdk12+

--- src/main/java/net/rezxis/mctp/client/ReflectionUtils.java
+++ src/main/java/net/rezxis/mctp/client/ReflectionUtils.java
@@ -1,6 +1,10 @@
 package net.rezxis.mctp.client;
 
+import sun.misc.Unsafe;
+
+import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
 public class ReflectionUtils {
@@ -9,44 +13,67 @@ public class ReflectionUtils {
 	private static final Field FIELD_ACCESSSOR;
 	private static final Field FIELD_ACCESSSOR_OVERRIDE;
 	private static final Field FIELD_ROOT;
+	private static final Unsafe unsafe;
+	private static final int version = getVersion();
 
 	static {
-		Field fieldModifiers = null;
-		Field fieldAccessor = null;
-		Field fieldAccessorOverride = null;
-		Field fieldRoot = null;
-		try {
-			fieldModifiers = Field.class.getDeclaredField("modifiers");
-			fieldModifiers.setAccessible(true);
-			fieldAccessor = Field.class.getDeclaredField("fieldAccessor");
-			fieldAccessor.setAccessible(true);
-			fieldAccessorOverride = Field.class.getDeclaredField("overrideFieldAccessor");
-			fieldAccessorOverride.setAccessible(true);
-			fieldRoot = Field.class.getDeclaredField("root");
-			fieldRoot.setAccessible(true);
-		} catch (Exception exception) {
-			exception.printStackTrace();
+		if (version >= 12) {
+			FIELD_MODIFIERS = null;
+			FIELD_ACCESSSOR = null;
+			FIELD_ACCESSSOR_OVERRIDE = null;
+			FIELD_ROOT = null;
+			Unsafe us = null;
+			try {
+				Field unsafeField = Unsafe.class.getDeclaredField("theUnsafe");
+				unsafeField.setAccessible(true);
+				us = (Unsafe) unsafeField.get(null);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+			unsafe = us;
+		} else {
+			Field fieldModifiers = null;
+			Field fieldAccessor = null;
+			Field fieldAccessorOverride = null;
+			Field fieldRoot = null;
+			try {
+				fieldModifiers = Field.class.getDeclaredField("modifiers");
+				fieldModifiers.setAccessible(true);
+				fieldAccessor = Field.class.getDeclaredField("fieldAccessor");
+				fieldAccessor.setAccessible(true);
+				fieldAccessorOverride = Field.class.getDeclaredField("overrideFieldAccessor");
+				fieldAccessorOverride.setAccessible(true);
+				fieldRoot = Field.class.getDeclaredField("root");
+				fieldRoot.setAccessible(true);
+			} catch (Exception exception) {
+				exception.printStackTrace();
+			}
+			FIELD_MODIFIERS = fieldModifiers;
+			FIELD_ACCESSSOR = fieldAccessor;
+			FIELD_ACCESSSOR_OVERRIDE = fieldAccessorOverride;
+			FIELD_ROOT = fieldRoot;
+			unsafe = null;
 		}
-		FIELD_MODIFIERS = fieldModifiers;
-		FIELD_ACCESSSOR = fieldAccessor;
-		FIELD_ACCESSSOR_OVERRIDE = fieldAccessorOverride;
-		FIELD_ROOT = fieldRoot;
 	}
 
 	public static void setFinalField(Class<?> objectClass, Object object, String fieldName, Object value) throws Exception {
 		Field field = objectClass.getDeclaredField(fieldName);
-		field.setAccessible(true);
+		if (version >= 12) {
+			long offset = unsafe.objectFieldOffset(field);
+			unsafe.putObject(object, offset, value);
+		} else {
+			field.setAccessible(true);
+			if (Modifier.isFinal(field.getModifiers())) {
+				FIELD_MODIFIERS.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+				Field currentField = field;
+				do {
+					FIELD_ACCESSSOR.set(currentField, null);
+					FIELD_ACCESSSOR_OVERRIDE.set(currentField, null);
+				} while ((currentField = (Field) FIELD_ROOT.get(currentField)) != null);
+			}
 
-		if (Modifier.isFinal(field.getModifiers())) {
-			FIELD_MODIFIERS.setInt(field, field.getModifiers() & ~Modifier.FINAL);
-			Field currentField = field;
-			do {
-				FIELD_ACCESSSOR.set(currentField, null);
-				FIELD_ACCESSSOR_OVERRIDE.set(currentField, null);
-			} while((currentField = (Field) FIELD_ROOT.get(currentField)) != null);
+			field.set(object, value);
 		}
-		
-		field.set(object, value);
 	}
 
 	@SuppressWarnings("unchecked")
@@ -56,4 +83,13 @@ public class ReflectionUtils {
 		return (T) field.get(object);
 	}
 
+	private static int getVersion() {
+		String version = System.getProperty("java.version");
+		if(version.startsWith("1.")) {
+			version = version.substring(2, 3);
+		} else {
+			int dot = version.indexOf(".");
+			if(dot != -1) { version = version.substring(0, dot); }
+		} return Integer.parseInt(version);
+	}
 }
--- src/main/java/net/rezxis/mctp/client/netty/NettyChannelInitializer.java
+++ src/main/java/net/rezxis/mctp/client/netty/NettyChannelInitializer.java
@@ -1,9 +1,6 @@
 package net.rezxis.mctp.client.netty;
 
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.ChannelInitializer;
+import io.netty.channel.*;
 import io.netty.channel.socket.SocketChannel;
 import net.rezxis.mctp.client.haproxy.HAProxyMessage;
 import net.rezxis.mctp.client.haproxy.HAProxyMessageDecoder;
@@ -12,6 +9,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
+import java.util.Map;
 
 import org.bukkit.Bukkit;
 
@@ -21,17 +19,17 @@ public class NettyChannelInitializer extends ChannelInitializer<SocketChannel> {
 	private static Field fieldL;
 	private ChannelInitializer<SocketChannel> oldChildHandler;
 	private Method oldChildHandlerMethod;
-	
+
 	public NettyChannelInitializer(ChannelInitializer<SocketChannel> oldChildHandler) throws Exception {
 		this.oldChildHandler = oldChildHandler;
 		this.oldChildHandlerMethod = this.oldChildHandler.getClass().getDeclaredMethod("initChannel", Channel.class);
 		this.oldChildHandlerMethod.setAccessible(true);
 	}
-	
+
 	@Override
 	protected void initChannel(SocketChannel channel) throws Exception {
 		this.oldChildHandlerMethod.invoke(this.oldChildHandler, channel);
-		
+
 		if (channel.pipeline().get("haproxy-decoder") == null )
 			channel.pipeline().addAfter("timeout", "haproxy-decoder", new HAProxyMessageDecoder());
 		if (channel.pipeline().get("haproxy-handler") == null )
@@ -42,7 +40,7 @@ public class NettyChannelInitializer extends ChannelInitializer<SocketChannel> {
                     HAProxyMessage message = (HAProxyMessage) msg;
                     String realaddress = message.sourceAddress();
                     int realport = message.sourcePort();
-                    
+
                     SocketAddress socketaddr = new InetSocketAddress(realaddress, realport);
 
             		fieldL.set(channel.pipeline().get("packet_handler"), socketaddr);
@@ -52,10 +50,19 @@ public class NettyChannelInitializer extends ChannelInitializer<SocketChannel> {
             }
 		});
 	}
-	
+
 	static {
+		Class<?> NetworkManagerClz = null;
 		try {
-			Class<?> NetworkManagerClz = Class.forName("net.minecraft.server."+NMS_VERSION+".NetworkManager");
+			NetworkManagerClz = Class.forName("net.minecraft.server."+NMS_VERSION+".NetworkManager");
+		} catch (Exception e) {
+			try {
+				NetworkManagerClz = Class.forName("net.minecraft.network.NetworkManager");
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		try {
 			for (Field field : NetworkManagerClz.getDeclaredFields()) {
 				if (field.getType().getName().contains("java.net.SocketAddress")) {
 					fieldL = field;
